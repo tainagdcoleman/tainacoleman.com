ea70b4690f4433b9a3e94a68c77fe883108fe462
Jared Coleman <jaredraycoleman@gmail.com>
2024-03-01 19:59:20 -0800

    Updates everything

--- app.py
+++ app.py
@@ -15,22 +15,7 @@ if os.getenv('FLASK_ENV') == 'production':
 @app.route('/')
 def home():
     """Renders the home page."""
-    return render_template('index.html')
-
-@app.route('/publications')
-def publications():
-    """Renders the publications page."""
-    return render_template('publications.html', publications=pubs, websites=websites)
-
-@app.route('/projects')
-def projects():
-    """Renders the projects page."""
-    return render_template('projects.html', projects=projs)
-
-@app.route('/about')
-def about():
-    """Renders the about page."""
-    return render_template('about.html')
+    return render_template('index.html', publications=pubs, websites=websites)
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
